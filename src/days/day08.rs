9759da800c5b8b54ad627fa829b771e18969f4d8
Maxwell L-T <maxwell.lt@live.com>
2023-12-08 22:28:29 -0500

    Solution for 2023 8-1 and 8-2
    Part 1 was simple to implement, just using a HashMap to move through the
    nodes worked well. This could be optimized by using a HashMap of
    randomly assigned numeric IDs in place of the strings (plus a marker for
    initial/terminal node status), as my solution does a ton of string
    copying.
    
    Part 2's brute force solution was infeasible, so I ended up looking for
    a hint, and learned that the number of steps for each path between
    terminal nodes was the same for every loop; hence finding the lowest
    common multiple of each path's loop length would suffice to solve this
    problem.
    
    Total solve time: 54m (33m + 21m)

--- src/days/day08.rs
+++ src/days/day08.rs
@@ -1,14 +1,127 @@
+use std::collections::HashMap;
+
+use itertools::Itertools;
+use num::Integer;
+
 use crate::solver::Solver;
 
 pub struct Day08;
 impl Solver for Day08 {
     #[allow(unused)]
     fn solve1(&self, input: &str) -> Option<i64> {
-        None
+        let mut parts = input.split("\n\n");
+        let sequence = parts
+            .next()
+            .unwrap()
+            .trim()
+            .chars()
+            .map(|c| match c {
+                'L' => 0,
+                'R' => 1,
+                _ => unimplemented!("Got direction {}!", c),
+            })
+            .collect_vec();
+        let nodes = parts
+            .next()
+            .unwrap()
+            .split("\n")
+            .filter(|s| !s.is_empty())
+            .map(parse_node)
+            .collect::<HashMap<_, _>>();
+        let mut current = String::from("AAA");
+        let mut steps: i64 = 0;
+        loop {
+            if current == "ZZZ" {
+                break;
+            }
+            current =
+                nodes.get(&current).unwrap()[sequence[steps as usize % sequence.len()]].to_owned();
+            steps += 1;
+        }
+        Some(steps)
     }
 
     #[allow(unused)]
     fn solve2(&self, input: &str) -> Option<i64> {
-        None
+        let mut parts = input.split("\n\n");
+        let sequence = parts
+            .next()
+            .unwrap()
+            .trim()
+            .chars()
+            .map(|c| match c {
+                'L' => 0,
+                'R' => 1,
+                _ => unimplemented!("Got direction {}!", c),
+            })
+            .collect_vec();
+        let nodes = parts
+            .next()
+            .unwrap()
+            .split("\n")
+            .filter(|s| !s.is_empty())
+            .map(parse_node)
+            .collect::<HashMap<_, _>>();
+        let mut current = nodes
+            .keys()
+            .filter(|s| s.chars().last().unwrap() == 'A')
+            .collect_vec();
+        let mut steps: i64 = 0;
+        current.iter()
+            .map(|current| {
+                let mut current = current.to_owned();
+                let mut steps: i64 = 0;
+                loop {
+                    if current.chars().last().unwrap() == 'Z' { break; }
+                    current = &nodes.get(current).unwrap()[sequence[steps as usize % sequence.len()]];
+                    steps += 1;
+                }
+                return steps;
+            })
+        .reduce(|a: i64, b: i64| a.lcm(&b))
+    }
+}
+
+fn parse_node(input: &str) -> (String, [String; 2]) {
+    let key = input[0..3].to_owned();
+    let values = [input[7..10].to_owned(), input[12..15].to_owned()];
+    (key, values)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1() {
+        let input = r#"RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)
+"#;
+        let output = Day08.solve1(input);
+        assert_eq!(output, Some(2));
+    }
+
+    #[test]
+    fn part2() {
+        let input = r#"LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+"#;
+        let output = Day08.solve2(input);
+        assert_eq!(output, Some(6))
     }
 }
