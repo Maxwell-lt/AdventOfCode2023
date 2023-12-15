f536be9d65957f86ee2ae97b391f6405a3786a10
Maxwell L-T <maxwell.lt@live.com>
2023-12-14 23:45:07 -0500

    Solution for 2023 14-2
    An hour or so later, I figured out the trick: the platform is bound to
    fall into a loop well before 1 billion cycles. If I can track each
    platform state along with the iteration count, and detect when the
    platform's current state matches a past cycle, I can then figure out
    which step of the loop will be encountered at iteration 1 billion.
    
    This was simple to implement, and once I had it in place I was able to
    see just how broken my old part 2 code was. This took a bit more time to
    solve, because every single day I put myself into a mess of array index
    math mistakes.
    
    In the end, this now takes <20ms to execute instead of 10 hours, and as
    a bonus will actually give the correct answer at the end!
    
    Total solve time: 2h (30m + [36m + 54m])

--- src/days/day14.rs
+++ src/days/day14.rs
@@ -1,4 +1,4 @@
-use std::time::Instant;
+use std::{collections::HashMap, hash::BuildHasher, time::Instant};
 
 use itertools::Itertools;
 
@@ -36,8 +36,22 @@ impl Solver for Day14 {
     fn solve2(&self, input: &str) -> Option<i64> {
         let mut platform = parse_platform(input);
         let start = Instant::now();
-        for i in 0..1_000_000_000 {
+        let mut hashes = HashMap::new();
+        let hasher = std::collections::hash_map::RandomState::new();
+        for i in 1..=1_000_000_000 {
             perform_roll(&mut platform);
+            let current_hash = hasher.hash_one(&platform);
+            if hashes.contains_key(&current_hash) {
+                let cycle_len = i - hashes.get(&current_hash).unwrap();
+                let cycles_til_end = 1_000_000_000 - i;
+                let additional_runs = cycles_til_end % cycle_len;
+                for _ in 0..additional_runs {
+                    perform_roll(&mut platform);
+                }
+                break;
+            } else {
+                hashes.insert(current_hash, i);
+            }
         }
         let height = platform.len();
         let mut load = 0;
@@ -86,17 +100,21 @@ fn perform_roll(platform: &mut Vec<Vec<Space>>) {
         for row in 0..height {
             if platform[row][column] == Space::Rolling {
                 rolling_found += 1;
+                platform[row][column] = Space::Empty;
             }
             if row == height - 1 || platform[row][column] == Space::Rock {
                 let mut placed = 0;
-                for edit_row in last_empty..row {
-                    if placed < rolling_found {
-                        platform[edit_row][column] = Space::Rolling;
-                        placed += 1;
-                    } else {
-                        platform[edit_row][column] = Space::Empty;
+                for edit_row in last_empty..=row {
+                    if platform[edit_row][column] != Space::Rock {
+                        if placed < rolling_found {
+                            platform[edit_row][column] = Space::Rolling;
+                            placed += 1;
+                        } else {
+                            platform[edit_row][column] = Space::Empty;
+                        }
                     }
                 }
+                rolling_found = 0;
                 last_empty = row + 1;
             }
         }
@@ -109,17 +127,21 @@ fn perform_roll(platform: &mut Vec<Vec<Space>>) {
         for column in 0..width {
             if platform[row][column] == Space::Rolling {
                 rolling_found += 1;
+                platform[row][column] = Space::Empty;
             }
             if column == width - 1 || platform[row][column] == Space::Rock {
                 let mut placed = 0;
-                for edit_column in last_empty..column {
-                    if placed < rolling_found {
-                        platform[row][edit_column] = Space::Rolling;
-                        placed += 1;
-                    } else {
-                        platform[row][edit_column] = Space::Empty;
+                for edit_column in last_empty..=column {
+                    if platform[row][edit_column] != Space::Rock {
+                        if placed < rolling_found {
+                            platform[row][edit_column] = Space::Rolling;
+                            placed += 1;
+                        } else {
+                            platform[row][edit_column] = Space::Empty;
+                        }
                     }
                 }
+                rolling_found = 0;
                 last_empty = column + 1;
             }
         }
@@ -132,17 +154,21 @@ fn perform_roll(platform: &mut Vec<Vec<Space>>) {
         for row in (0..height).rev() {
             if platform[row][column] == Space::Rolling {
                 rolling_found += 1;
+                platform[row][column] = Space::Empty;
             }
             if row == 0 || platform[row][column] == Space::Rock {
                 let mut placed = 0;
-                for edit_row in last_empty..row {
-                    if placed < rolling_found {
-                        platform[edit_row][column] = Space::Rolling;
-                        placed += 1;
-                    } else {
-                        platform[edit_row][column] = Space::Empty;
+                for edit_row in (row..=last_empty).rev() {
+                    if platform[edit_row][column] != Space::Rock {
+                        if placed < rolling_found {
+                            platform[edit_row][column] = Space::Rolling;
+                            placed += 1;
+                        } else {
+                            platform[edit_row][column] = Space::Empty;
+                        }
                     }
                 }
+                rolling_found = 0;
                 if row != 0 {
                     last_empty = row - 1;
                 }
@@ -152,22 +178,26 @@ fn perform_roll(platform: &mut Vec<Vec<Space>>) {
     // Eastward
     for row in 0..platform.len() {
         let width = platform[0].len();
-        let mut last_empty = 0;
+        let mut last_empty = width - 1;
         let mut rolling_found = 0;
         for column in (0..width).rev() {
-            if platform[column][row] == Space::Rolling {
+            if platform[row][column] == Space::Rolling {
                 rolling_found += 1;
+                platform[row][column] = Space::Empty;
             }
-            if column == 0 || platform[column][row] == Space::Rock {
+            if column == 0 || platform[row][column] == Space::Rock {
                 let mut placed = 0;
-                for edit_column in last_empty..column {
-                    if placed < rolling_found {
-                        platform[row][edit_column] = Space::Rolling;
-                        placed += 1;
-                    } else {
-                        platform[row][edit_column] = Space::Empty;
+                for edit_column in (column..=last_empty).rev() {
+                    if platform[row][edit_column] != Space::Rock {
+                        if placed < rolling_found {
+                            platform[row][edit_column] = Space::Rolling;
+                            placed += 1;
+                        } else {
+                            platform[row][edit_column] = Space::Empty;
+                        }
                     }
                 }
+                rolling_found = 0;
                 if column != 0 {
                     last_empty = column - 1;
                 }
@@ -198,10 +228,9 @@ O.#..O.#.#
         assert_eq!(output, Some(136));
     }
 
-    // Commented out because it will be painfully slow
-    //#[test]
-    //fn part2() {
-    //    let output = Day14.solve2(EXAMPLE);
-    //    assert_eq!(output, Some(64));
-    //}
+    #[test]
+    fn part2() {
+        let output = Day14.solve2(EXAMPLE);
+        assert_eq!(output, Some(64));
+    }
 }
